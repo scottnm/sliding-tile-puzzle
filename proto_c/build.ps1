aca294463463626bab0ab83bf134a1a0e86ed096
Scott Munro <scottnmunro@gmail.com>
2021-06-02 10:33:46 -0700

    slide starter

---
+++ proto_c/build.ps1
@@ -0,0 +1,17 @@
+param(
+    [switch]$Run
+    )
+
+cl /TC .\src\*.c /I .\src\ /W4 /Z7 /nologo /Fo:.\obj\ /Fe:.\bin\slide.exe
+
+if (!$?)
+{
+    Write-Warning "Failed to compile!"
+    return;
+}
+
+if ($Run)
+{
+    Write-Host -ForegroundColor Cyan "`nRunning:"
+    .\bin\slide.exe
+}
