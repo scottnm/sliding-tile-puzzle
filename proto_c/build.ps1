840c3a1d920ab2f46cd6727db7b68dc60b9c87ae
scott munro <scottnmunro@gmail.com>
2022-05-15 08:59:03 -0700

    fix build script to make directories for me

--- proto_c/build.ps1
+++ proto_c/build.ps1
@@ -2,6 +2,11 @@ param(
     [switch]$Run
     )
 
+rm -r .\obj\* -ErrorAction SilentlyContinue
+rm -r .\bin\* -ErrorAction SilentlyContinue
+mkdir obj -ErrorAction SilentlyContinue
+mkdir bin -ErrorAction SilentlyContinue
+
 cl /TC .\src\*.c /I .\src\ /W4 /WX /Z7 /nologo /Fo:.\obj\ /Fe:.\bin\slide.exe
 
 if (!$?)
